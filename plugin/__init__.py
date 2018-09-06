2ae20230d399710bdde28e769885e11c647191f6
mra <m.rau@plan-net.com>
2018-09-06 08:43:00 +0200

    added description to plugin __init__.py file

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -4,3 +4,7 @@ __project__ = 'core4'
 __title__ = 'plugin example (core4 regression tests)'
 __version__ = None
 __built__ = None
+__description__ = """
+This is a plugin example used in core4 regression tests. Use this __init__.py
+package file as a blueprint to new plugins.
+"""
