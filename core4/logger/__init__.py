c40b7c4840472cdcd3d5a69ed19336963fb7eeb3
Michael Rau <m.rau@plan-net.com>
2018-10-04 06:37:24 +0200

    introduced helper function logon

--- core4/logger/__init__.py
+++ core4/logger/__init__.py
@@ -1,3 +1,3 @@
 from core4.logger.adapter import CoreLoggingAdapter
 from core4.logger.exception import ExceptionHandler
-from core4.logger.mixin import CoreLoggerMixin
+from core4.logger.mixin import CoreLoggerMixin, logon
