053c2f4cfe8fc16ee4d3d443a5f783cfc69d75e7
Michael Rau <m.rau@plan-net.com>
2018-10-23 23:22:02 +0200

    login success (async)

--- core4/api/v1/request/login.py
+++ core4/api/v1/request/login.py
@@ -2,7 +2,7 @@ import base64
 
 import datetime
 import jwt
-
+import motor
 import core4.util
 from core4.api.v1.request.main import CoreRequestHandler
 from core4.api.v1.role.main import Role
@@ -11,14 +11,18 @@ from core4.api.v1.role.main import Role
 class LoginHandler(CoreRequestHandler):
     protected = False
 
-    def get(self):
-        self.post()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.motor = motor.MotorClient("mongodb://core:654321@localhost:27017")
 
-    def post(self):
-        username = self.verify_user()
+    async def get(self):
+        await self.post()
+
+    async def post(self):
+        username = await self.verify_user()
         if username:
             token = self._create_token(username)
-            self.set_current_user(username)
+            self.current_user = username
             return self.reply({
                 "token": token
             })
@@ -38,3 +42,41 @@ class LoginHandler(CoreRequestHandler):
         }
         token = jwt.encode(payload, secret, algorithm)
         return token.decode("utf-8")
+
+    async def verify_user(self):
+        auth_header = self.request.headers.get('Authorization')
+        username = password = None
+        token = None
+        if auth_header is not None:
+            auth_type = auth_header.split()[0].lower()
+            if auth_type == "basic":
+                auth_decoded = base64.decodebytes(
+                    auth_header[6:].encode("utf-8"))
+                username, password = auth_decoded.decode(
+                    "utf-8").split(':', 2)
+            elif auth_type == "bearer":
+                token = auth_header[7:]
+        else:
+            token = self.get_argument("token", default=None)
+            username = self.get_argument("username", None)
+            password = self.get_argument("password", None)
+        if token:
+            payload = self._parse_token(token)
+            return payload.get("name")
+        if username and password:
+            try:
+                user = await self.load_user(username)
+                #user = Role().load_one(name=username)
+            except:
+                self.logger.warning("username [%s] not found", username)
+            else:
+                if user.verify_password(password):
+                    #user.login()
+                    return username
+        return None
+
+    async def load_user(self, username):
+        doc = await self.motor.core4test.sys.role.find_one({'name': username})
+        doc["password_hash"] = doc["password"]
+        del doc["password"]
+        return Role(**doc)
