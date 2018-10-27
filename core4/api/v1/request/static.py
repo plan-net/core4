40db787a0eb7473b86cb4f045542c0a191c57bb5
Michael Rau <m.rau@plan-net.com>
2018-10-27 18:32:09 +0200

    add request id to 404 (handler not found)

--- core4/api/v1/request/static.py
+++ core4/api/v1/request/static.py
@@ -1,9 +1,20 @@
-from tornado.web import StaticFileHandler
+from tornado.web import StaticFileHandler, RequestHandler
 
 from core4.api.v1.request.main import BaseHandler
 
 
 class CoreStaticFileHandler(BaseHandler, StaticFileHandler):
+
+    def __init__(self, *args, **kwargs):
+        BaseHandler.__init__(self)
+        StaticFileHandler.__init__(self, *args, **kwargs)
+
+    def verify_access(self):
+        return True
+
+
+
+class CoreDynamicFileHandler(BaseHandler, RequestHandler):
 
     def __init__(self, *args, **kwargs):
         BaseHandler.__init__(self)
