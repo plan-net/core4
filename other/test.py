4598b2c6585578e750649862fd1afa5193329b1b
Michael Rau <m.rau@plan-net.com>
2018-10-30 21:09:14 +0100

    user own module other instead of requests to test custom module logging

---
+++ other/test.py
@@ -0,0 +1,11 @@
+
+import logging
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.warning('Watch out!')  # will print a message to the console
+logger.info('I told you so')  # will not print anything
+logger.error('finally')  # will not print anything
+print("OK")
