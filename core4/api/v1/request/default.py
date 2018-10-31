a40f21350cd8aa57ca46c932d828ed6b2e7199f2
Michael Rau <m.rau@plan-net.com>
2018-10-31 13:40:55 +0100

    refactored import

--- core4/api/v1/request/default.py
+++ core4/api/v1/request/default.py
@@ -1,8 +1,8 @@
-import core4.api.v1.request.main
+from core4.api.v1.request.main import CoreRequestHandler
 import tornado.web
 
 
-class DefaultHandler(core4.api.v1.request.main.CoreRequestHandler):
+class DefaultHandler(CoreRequestHandler):
 
     def initialize(self, *args, status_code=404, **kwargs):
         self.set_status(status_code)
