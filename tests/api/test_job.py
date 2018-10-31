cb651436b4936dd83df7fad08e8ffe006eb59195
Michael Rau <m.rau@plan-net.com>
2018-10-31 13:42:17 +0100

    added required cookie_secret, removed obsolete test_api, now in /api

--- tests/api/test_job.py
+++ tests/api/test_job.py
@@ -41,6 +41,7 @@ def setup(tmpdir):
     os.environ["CORE4_OPTION_logging__mongodb"] = "DEBUG"
     os.environ["CORE4_OPTION_api__token__expiration"] = "!!int 60"
     os.environ["CORE4_OPTION_api__setting__debug"] = "!!bool False"
+    os.environ["CORE4_OPTION_api__setting__cookie_secret"] = "blabla"
     os.environ["CORE4_OPTION_worker__min_free_ram"] = "!!int 32"
     conn = pymongo.MongoClient(MONGO_URL)
     conn.drop_database(MONGO_DATABASE)
