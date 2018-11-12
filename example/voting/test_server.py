fc8dd5bdb849834254e012dc85461db6503e28f1
Michael Rau <m.rau@plan-net.com>
2018-11-12 22:55:24 +0100

    continued implementation of example, now with pytest

--- example/voting/test_server.py
+++ example/voting/test_server.py
@@ -1,111 +1,273 @@
-from requests import get, post, delete, put
-from pprint import pprint
+import os
+
+import pymongo
+import pytest
+from threading import Thread
+
+import core4.service.setup
+import core4.util.data
+import core4.util.tool
+from core4.api.v1.test import ClientServer
+from example.voting.server import VotingApp
+
+MONGO_URL = 'mongodb://core:654321@localhost:27017'
+MONGO_DATABASE = 'voting_test'
 
 
-URL = "http://localhost:5001/voting/v1"
+@pytest.fixture(autouse=True)
+def setup(tmpdir):
+    os.environ["CORE4_OPTION_folder__root"] = str(tmpdir)
+    os.environ["CORE4_OPTION_DEFAULT__mongo_url"] = MONGO_URL
+    os.environ["CORE4_OPTION_logging__mongodb"] = "DEBUG"
+    os.environ["CORE4_OPTION_api__setting__debug"] = "!!bool True"
+    os.environ["CORE4_OPTION_api__setting__cookie_secret"] = "blabla"
+    os.environ[
+        "CORE4_OPTION_example__DEFAULT__mongo_database"] = MONGO_DATABASE
+    conn = pymongo.MongoClient(MONGO_URL)
+    conn.drop_database(MONGO_DATABASE)
+    core4.logger.mixin.logon()
+    yield
+    # conn.drop_database(MONGO_DATABASE)
+    for i, j in core4.service.setup.CoreSetup.__dict__.items():
+        if callable(j):
+            if "has_run" in j.__dict__:
+                j.has_run = False
+    core4.util.tool.Singleton._instances = {}
+    dels = []
+    for k in os.environ:
+        if k.startswith('CORE4_'):
+            dels.append(k)
+    for k in dels:
+        del os.environ[k]
 
-def register():
-    rv = post(
-        URL + "/register",
-        json={
+
+@pytest.fixture()
+def http():
+    server = ClientServer(VotingApp)
+    server.token = None
+    yield server
+    server.stop()
+
+
+def test_init(http):
+    rv = http.get("/profile")
+    assert rv.status_code == 401
+
+
+def test_register(http):
+    rv = http.post("/register", json={"token": "secret_token", "id": "mra"})
+    assert rv.status_code == 200
+
+
+def test_create_session(http):
+    rv = http.post("/session", json={"token": "secret_token",
+                                     "data": {"label": "Feeling"}})
+    assert rv.status_code == 400
+    assert "Missing argument question" in rv.json()["error"]
+
+    rv = http.post("/session", json={"token": "secret_token",
+                                     "question": "Do you feel good?",
+                                     "data": "bla"})
+    assert rv.status_code == 400
+    assert "parameter [data] expected as_type [dict]" in rv.json()["error"]
+
+    rv = http.post("/session", json={"token": "secret_token",
+                                     "question": "Do you feel good?",
+                                     "data": {"label": "Feeling"}})
+    assert rv.status_code == 200
+    rv = http.post("/session", json={"token": "secret_token",
+                                     "question": "Do you feel good?",
+                                     "data": {"label": "Feeling"}})
+    assert rv.status_code == 200
+
+
+def test_session_listing(http):
+    for i in range(1, 5):
+        rv = http.post("/session", json={
             "token": "secret_token",
-            "_id": "mra"
-        }
-    )
-    print(rv.json())
+            "question": "Do you like number %d?" % (i),
+            "data": {"label": "Number %d" % (i)}})
+        assert rv.status_code == 200
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+    assert 4 == len(rv.json()["data"])
 
 
-def create_session(question, data={}):
-    rv = post(
-        URL + "/session",
-        json={
-            "token": "secret_token",
-            "question": question,
-            "data": data
-        }
-    )
-    print(rv.json())
+def helper_session_0(http):
+    test_session_listing(http)
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+    session = rv.json()["data"][0]
+    rv = http.get("/session/" + session["session_id"],
+                  json={"token": "secret_token"})
+    assert rv.status_code == 200
+    data = rv.json()["data"]
+    return data
 
 
-#register()
-create_session("This is the meeting question", data={"tag": "test"})
-
-def listing():
-    rv = get(URL + "/session", json={"token": "secret_token"})
-    pprint(rv.json())
-
-def detail(sid):
-    rv = get(URL + "/session/" + sid, json={"token": "secret_token"})
-    pprint(rv.json())
-
-def open_session(sid):
-    rv = post(URL + "/start/" + sid, json={"token": "secret_token"})
-    pprint(rv.json())
+def test_session_detail(http):
+    data = helper_session_0(http)
+    data.pop("created_at")
+    assert data == {
+        'session_id': data["session_id"],
+        'question': 'Do you like number 1?',
+        'data': {
+            'label': 'Number 1'
+        },
+        'state': 'CLOSED'
+    }
 
 
-sid = "5be8ad3bde8b694326bce5ff"
-#listing()
-#detail(sid)
+def test_open_session(http):
+    data = helper_session_0(http)
+    rv = http.post("/start/" + data["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+    rv = http.get("/session/" + data["session_id"],
+                  json={"token": "secret_token"})
+    assert rv.status_code == 200
+    data = rv.json()["data"]
+    assert data["state"] == "OPEN"
 
-#open_session(sid)
+    rv = http.post("/start/" + data["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
 
-def create_session2():
-    rv = post(
-        URL + "/session",
-        json={
-            "token": "secret_token",
-            "question": "Another question?",
-        }
-    )
-    pprint(rv.json())
-
-
-#create_session2()
-#listing()
-
-sid2 = "5be8ae65de8b694326bce642"
-#open_session(sid)
-#open_session(sid)
-#listing()
+    rv = http.get("/session/" + data["session_id"],
+                  json={"token": "secret_token"})
+    assert rv.status_code == 200
+    data = rv.json()["data"]
+    assert data["state"] == "OPEN"
+    return data
 
 
-def close_session(sid):
-    rv = post(
-        URL + "/stop/" + sid,
-        json={
-            "token": "secret_token"
-        }
-    )
-    pprint(rv.json())
-
-#close_session(sid)
-#listing()
-#detail(sid)
-
-def event(id, data={}):
-    rv = post(
-        URL + "/event",
-        json={
-            "id": id,
-            "token": "secret_token",
-            "data": data
-        }
-    )
-    pprint(rv.json())
-
-#event("mra")
-
-#create_session("My final question")
-#open_session("5be958b8de8b6975631ff7f0")
-#listing()
-
-#event("mra")
-#event("bzi1")
+def test_unknown_session(http):
+    sid = "5be9e1e7de8b6958c33b8e1f"
+    rv = http.post("/start/" + sid,
+                   json={"token": "secret_token"})
+    assert rv.status_code == 404
+    rv = http.get("/session/" + sid,
+                  json={"token": "secret_token"})
+    assert rv.status_code == 404
 
 
+def test_close_session(http):
+    data = test_open_session(http)
+    rv = http.post("/start/" + data["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
 
-#open_session("5be958b8de8b6975631ff7f0")
-#event("mra2")
-#event("bzi2")
+    rv = http.post("/stop/" + data["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
 
-#close_session("5be958b8de8b6975631ff7f0")
+    rv = http.get("/session/" + data["session_id"],
+                  json={"token": "secret_token"})
+    assert rv.status_code == 200
+    data = rv.json()["data"]
+    assert data["state"] == "CLOSED"
+
+
+def test_4_session(http):
+    test_session_listing(http)
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+    data = rv.json()["data"]
+    return data
+
+
+def test_open_other(http):
+    data = test_4_session(http)
+
+    rv = http.post("/start/" + data[0]["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+    assert (['OPEN', 'CLOSED', 'CLOSED', 'CLOSED']
+            == [i["state"] for i in rv.json()["data"]])
+
+    rv = http.post("/start/" + data[2]["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+    assert (['CLOSED', 'CLOSED', 'OPEN', 'CLOSED']
+            == [i["state"] for i in rv.json()["data"]])
+
+
+def test_event(http):
+    data = test_4_session(http)
+
+    rv = http.post("/start/" + data[0]["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    rv = http.get("/session", json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    for i in range(10):
+        rv = http.post(
+            "/event",
+            json={"token": "secret_token", "id": "user-%d" % (i + 1)})
+        assert rv.status_code == 201
+
+    for i in range(10):
+        rv = http.post(
+            "/event",
+            json={"token": "secret_token", "id": "user-%d" % (i + 1)})
+        assert rv.status_code == 200
+
+    rv = http.post(
+        "/event", json={"token": "secret_token", "id": "user-99"})
+    assert rv.status_code == 201
+
+    rv = http.post(
+        "/event", json={"token": "secret_token", "id": "user-99"})
+    assert rv.status_code == 200
+
+    rv = http.post("/stop/" + data[0]["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    rv = http.post(
+        "/event", json={"token": "secret_token", "id": "user-99"})
+    assert rv.status_code == 404
+
+
+def test_polling(http):
+    data = test_4_session(http)
+
+    rv = http.post("/start/" + data[0]["session_id"],
+                   json={"token": "secret_token"})
+    assert rv.status_code == 200
+
+    def make_events():
+        for i in range(50):
+            rv = http.post(
+                "/event",
+                json={"token": "secret_token", "id": "user-%d" % (i + 1)})
+            assert rv.status_code == 201
+        rv = http.post("/stop/" + data[0]["session_id"],
+                       json={"token": "secret_token"})
+        assert rv.status_code == 200
+
+    t = Thread(target=make_events)
+    t.start()
+    rv = http.get("/poll/" + data[0]["session_id"],
+                  json={"token": "secret_token"}, stream=True)
+    ret = []
+    for line in rv.iter_lines():
+        if line:
+            ret.append(core4.util.data.json_decode(line.decode("utf-8")))
+    t.join()
+    n = [i["n"] for i in ret]
+    assert n == sorted(n)
+    s = [i["state"] for i in ret]
+    assert set(s[:-1]) == {"OPEN"}
+    assert s[-1] == "CLOSED"
+    rv = http.get("/poll/" + data[0]["session_id"],
+                  json={"token": "secret_token"}, stream=True)
+    assert rv.json() == {'state': 'CLOSED', 'n': 50}
