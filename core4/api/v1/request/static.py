dac1b816965c1a2ab820fd73e50c01c5b5a648d6
Michael Rau <m.rau@plan-net.com>
2018-11-15 22:54:03 +0100

    initial (testing) version of dynamic/static file serving

--- core4/api/v1/request/static.py
+++ core4/api/v1/request/static.py
@@ -1,17 +1,18 @@
 from tornado.web import StaticFileHandler
 
-from core4.api.v1.request.main import BaseHandler
+from tornado.web import HTTPError
+import os
+from core4.api.v1.request.main import CoreRequestHandler
+#from core4.base.main import CoreBase
 
-
-class CoreStaticFileHandler(BaseHandler, StaticFileHandler):
+class CoreStaticFileHandler(CoreRequestHandler):
     """
     :class:`tornado.web.StaticFileHandler` based on
     :class:`core4.api.v1.main.BaseHandler`.
     """
 
-    def __init__(self, *args, **kwargs):
-        BaseHandler.__init__(self)
-        StaticFileHandler.__init__(self, *args, **kwargs)
+    protected = False
+    root = ""
 
     async def verify_access(self):
         """
@@ -20,3 +21,23 @@ class CoreStaticFileHandler(BaseHandler, StaticFileHandler):
         :return: ``True``
         """
         return True
+
+    def initialize(self, path=None, default_filename=None):
+        self.root = os.path.abspath(path or self.root)
+        if not self.root.endswith(os.path.sep):
+            self.root += os.path.sep
+        self.default_filename = default_filename
+
+    def head(self, path):
+        return self.get(path, include_body=False)
+
+    def get(self, path, include_body=True):
+        abspath = os.path.join(self.root, path)
+        if abspath.startswith(self.root):
+            if os.path.exists(abspath):
+                if include_body:
+                    self.render(abspath)
+                    return
+                else:
+                    assert self.request.method == "HEAD"
+        raise HTTPError(404, abspath)
