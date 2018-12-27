87bb8b4b3d18f11c6365ae3d949b7058c980fc85
Michael Rau <m.rau@plan-net.com>
2018-12-27 15:47:47 +0100

    wrapped croll.py

--- croll.py
+++ croll.py
@@ -26,30 +26,35 @@ def imp(mod):
         pipmain(["install", mod])
 
 
-script = sys.argv[0]
-try:
-    project = sys.argv[1]
-    remote = sys.argv[2]
-except:
-    raise AttributeError("usage: python3 croll.py PROJECT REPOSITORY")
+def main(script, project, remote, executable=None):
+    python_executable = executable or os.path.abspath(
+        os.path.join(project, "bin", "python"))
+    upgrade_script = os.path.abspath(
+        os.path.join(project, "bin", "requirements.txt"))
+    if not os.path.exists(python_executable):
+        print("install Python virtual environment in [{}]".format(project))
+        builder = venv.EnvBuilder(system_site_packages=False, clear=False,
+                                  symlinks=False, upgrade=False, with_pip=True)
+        builder.create(project)
 
-python_executable = os.path.abspath(os.path.join(project, "bin", "python"))
-upgrade_script = os.path.abspath(
-    os.path.join(project, "bin", "requirements.txt"))
-if not os.path.exists(python_executable):
-    print("install Python virtual environment in [{}]".format(project))
-    builder = venv.EnvBuilder(system_site_packages=False, clear=False,
-                              symlinks=False, upgrade=False, with_pip=True)
-    builder.create(project)
+    if len(sys.argv) == 3:
+        os.execl(
+            python_executable, python_executable, script, project, remote, "x")
+    else:
+        print("entering Python virtual environment in [{}]".format(project))
+        print("installing [{}] from [{}]".format(project, remote))
+        with open(upgrade_script, "w", encoding="utf-8") as fh:
+            fh.write("git+" + remote + "\n")
+            if project != "core4":
+                fh.write(CORE4_REMOTE + "\n")
+        pipmain(["install", "--requirement", upgrade_script])
 
-if len(sys.argv) == 3:
-    os.execl(
-        python_executable, python_executable, script, project, remote, "x")
-else:
-    print("entering Python virtual environment in [{}]".format(project))
-    print("installing [{}] from [{}]".format(project, remote))
-    with open(upgrade_script, "w", encoding="utf-8") as fh:
-        fh.write("git+" + remote + "\n")
-        if project != "core4":
-            fh.write(CORE4_REMOTE + "\n")
-    pipmain(["install", "--requirement", upgrade_script])
+
+if __name__ == '__main__':
+    script = sys.argv[0]
+    try:
+        project = sys.argv[1]
+        remote = sys.argv[2]
+    except:
+        raise AttributeError("usage: python3 croll.py PROJECT REPOSITORY")
+    main(script, project, remote)
