ac5f6d8b4a5a072c444daab98755e27d7601c6f0
Michael Rau <m.rau@plan-net.com>
2019-01-12 22:13:36 +0100

    added documentation of route endpoint (/info)

--- core4/api/v1/request/standard/route.py
+++ core4/api/v1/request/standard/route.py
@@ -19,39 +19,22 @@ class RouteHandler(CoreRequestHandler):
             page (int): requested page (start counting with 0)
             filter (dict): MongoDB query filter
             sort (list of tuple): with key and sort order
+            content_type (str): force json, html, text
 
         Returns:
             collection (list) of dict with
 
             - **_id** (*:class:`bson.objectid.ObjectId`*): MonboDB identifier
+            - **card_url** (*str*): full url to the card page view
+            - **help_url** (*str*): full url to the help page
             - **project** (*str*): name
-            - **args** (*list*): endpoint arguments passed to
-              :class:`.CoreApiContainer`
-            - **container** (*str*): ``qual_name`` of the
-              :class:`.CoreApiContainer`
-            - **author** (*str*): of the :class:`.CoreRequestHandler`
-            - **pattern** (*str*): of the route including regular expresions
-              groups
-            - **full_url** (*str*): to the endpoint
-            - **card_url** (*str*): to the card page view
-            - **help_url** (*str*): to the help page
-            - **description** (*str*): doc string
-            - **error** (*list*): of :mod:`docutils` parsing errors
-            - **host** (*str*): hostname and port
-            - **icon** (*str*): Material design icon label
-            - **protected** (*bool*): ``True`` if the handler requires
-              authentication and authorization
-            - **protocol** (*str*): ``http`` or ``https``
             - **qual_name** (*str*): of the :class:`.CoreRequestHandler`
             - **route_id** (*str*): MD5 digest of the route
+            - **routine** (*str*): url prefix with protocol and domain
+              adressing the serving container
             - **tag** (*list*): of tag lables assigned to the
               :class:`.CoreRequestHandler`
             - **title** (*str*): of the :class:`.CoreRequestHandler`
-            - **url** (*str*): route excluding regular expression groups
-            - **version** (*str*): of the project
-            - **visited** (*isoformat*): date/time when the
-              :class:`core4.queue.helper.job.ApiJob` recently visited the
-              endpoint
 
         Raises:
             401 Unauthorized:
@@ -62,69 +45,38 @@ class RouteHandler(CoreRequestHandler):
             >>> signin = get(url + "/login?username=admin&password=hans")
             >>> token = signin.json()["data"]["token"]
             >>> h = {"Authorization": "Bearer " + token}
-            >>> rv = get("http://localhost:5001/core4/api/v1/info/collection?per_page=2", headers=h)
+            >>> rv = get("http://localhost:5001/core4/api/v1/info?per_page=2", headers=h)
             >>> rv.json()
-            {
-                '_id': '5c0e7ab4de8b696c96d1b842',
-                'timestamp': '2018-12-10T14:39:48.100079',
-                'code': 200,
-                'message': 'OK',
-                'page': 0,
-                'count': 2,
-                'per_page': 2,
-                'page_count': 6,
-                'total_count': 12.0,
-                'data': [
-                    {
-                        '_id': '5c0da00a905eedc906935816',
-                        'args': [],
-                        'author': 'mra',
-                        'card_url': '/core4/api/v1/info/card/04e5f7635c0664033f920b028b20ef7a',
-                        'container': 'core4.api.v1.application.RootContainer',
-                        'description': '<p>None</p>\n',
-                        'error': [],
-                        'full_url': 'https://mra.devops:8080/core4/api/v1/info/collection',
-                        'help_url': '/core4/api/v1/info/04e5f7635c0664033f920b028b20ef7a',
-                        'host': 'mra.devops:8080',
-                        'icon': 'copyright',
-                        'pattern': '/core4/api/v1/info/collection',
-                        'project': 'core4',
-                        'protected': True,
-                        'protocol': 'https',
-                        'qual_name': 'core4.api.v1.request.standard.route.RouteHandler',
-                        'route_id': '04e5f7635c0664033f920b028b20ef7a',
-                        'tag': [],
-                        'title': 'core4 api/widget endpoint collection',
-                        'url': '/core4/api/v1/info/collection',
-                        'version': '0.0.1',
-                        'visited': '2018-12-10T10:19:12'
-                    },
-                    {
-                        '_id': '5c0da00a905eedc906935813',
-                        'args': [],
-                        'author': 'mra',
-                        'card_url': '/core4/api/v1/info/card/1baebadac9588d0c3c67e2728f47ee9b',
-                        'container': 'core4.api.v1.application.RootContainer',
-                        'description': '<p>None</p>\n',
-                        'error': [],
-                        'full_url': 'https://mra.devops:8080/core4/api/v1/profile',
-                        'help_url': '/core4/api/v1/info/1baebadac9588d0c3c67e2728f47ee9b',
-                        'host': 'mra.devops:8080',
-                        'icon': 'copyright',
-                        'pattern': '/core4/api/v1/profile',
-                        'project': 'core4',
-                        'protected': True,
-                        'protocol': 'https',
-                        'qual_name': 'core4.api.v1.request.standard.profile.ProfileHandler',
-                        'route_id': '1baebadac9588d0c3c67e2728f47ee9b',
-                        'tag': [],
-                        'title': 'details for the logged in user',
-                        'url': '/core4/api/v1/profile',
-                        'version': '0.0.1',
-                        'visited': '2018-12-10T10:19:12'
-                    }
-                ]
-            }
+            {'_id': '5c3a56fede8b695519da267b',
+             'code': 200,
+             'count': 2,
+             'data': [{
+               '_id': '5c3a56fbde8b695519da266b',
+               'card_url': 'http://mra.devops:5001/core4/api/v1/info/card/9f5123d239393f9aabd5f0968bb70e05',
+               'enter_url': 'http://mra.devops:5001/core4/api/v1/enter/9f5123d239393f9aabd5f0968bb70e05',
+               'help_url': 'http://mra.devops:5001/core4/api/v1/info/9f5123d239393f9aabd5f0968bb70e05',
+               'project': 'core4',
+               'qual_name': 'core4.api.v1.request.info.InfoHandler',
+               'route_id': '9f5123d239393f9aabd5f0968bb70e05',
+               'routing': 'http://mra.devops:5001',
+               'tag': [],
+               'title': 'server endpoint information'},
+              {'_id': '5c3a56fbde8b695519da266e',
+               'card_url': 'http://mra.devops:5001/core4/api/v1/info/card/3437b1b348dcce91f4949f4d6ad416aa',
+               'enter_url': 'http://mra.devops:5001/core4/api/v1/enter/3437b1b348dcce91f4949f4d6ad416aa',
+               'help_url': 'http://mra.devops:5001/core4/api/v1/info/3437b1b348dcce91f4949f4d6ad416aa',
+               'project': 'core4',
+               'qual_name': 'core4.api.v1.request.queue.job.JobHandler',
+               'route_id': '3437b1b348dcce91f4949f4d6ad416aa',
+               'routing': 'http://mra.devops:5001',
+               'tag': ['job management'],
+               'title': 'job manager'}],
+             'message': 'OK',
+             'page': 0,
+             'page_count': 6,
+             'per_page': 2,
+             'timestamp': '2019-01-12T21:07:10.464690',
+             'total_count': 12.0}
         """
 
         per_page = int(self.get_argument("per_page", default=10))
