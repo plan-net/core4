382e4b11c2199c2dc77b7945b832d3277c5dc05d
Michael Rau <m.rau@plan-net.com>
2019-01-13 14:07:24 +0100

    just hints

--- core4/script/cadmin.py
+++ core4/script/cadmin.py
@@ -1,11 +1,67 @@
 """
 cadmin - core4 administration utililty.
 
+deploy project in prod
+    upgrade project, core4 and packages
+
+    cadmin --install mypro==1.2.3 --repository
+    cadmin --upgrade
+
+    export CORE4_HOME=/tmp/core4.home
+    export CORE4_PROJECT=core4
+    export CORE4_PROJECT_BRANCH=mra.ops
+    export CORE4_REPOSITORY=ssh://git.bi.plan-net.com/srv/git/core4.git
+
+    SETUP
+
+        cd $CORE4_HOME
+        mkdir $CORE4_PROJECT
+        cd $CORE4_PROJECT
+        python3 -m venv .venv
+        . .venv/bin/activate
+        git clone git+$CORE4_REPOSITORY .repos
+        cd .repos
+        git checkout $BRANCH
+        pip install .
+        cd ..
+        rm -Rf .repos
+
+    UPGRADE
+
+        cd $CORE4_HOME
+        pip install
+
+    cd demo/voting/webapps/manager
+    yarn
+    yarn build
+    cd ../voting
+    yarn
+    yarn build
+    coco --app --filter demo.voting
+
+
+develop project
+    upgrade core4 and packages
+
+    cadmin --develop mypro
+
+develop core4
+    upgrade packages
+
+    cadmin --develop core4
+
+develop project and core4 together
+    upgrade packages
+
+    cadmin --develop mypro
+
+
+cadmin --upgrade --all
 
 
 Usage:
-  cadmin --project
-  cadmin --version
+  cadmin    --project
+  cadmin    --version
 
 Options:
   -h --help       Show this screen.
@@ -14,24 +70,11 @@ Options:
 
 from docopt import docopt
 
-import core4
-import core4.base.main
-import core4.service.introspect.project
-
-base = core4.base.main.CoreBase()
-
-
-def list_project():
-    pf = core4.service.introspect.project.CorePortfolio()
-    for (name, path, binary) in pf.list_project():
-        print(name)
-        print(pf.enter_project(name, binary))
-
 
 def main():
-    args = docopt(__doc__, help=True, version=core4.__version__)
+    args = docopt(__doc__, help=True)
     if args["--project"]:
-        list_project()
+        pass
     else:
         raise SystemExit("nothing to do.")
 
