4aeb3b898df66d31143e77a539e2bcd23049cfb7
Michael Rau <m.rau@plan-net.com>
2019-01-20 10:51:57 +0100

    bump release-0.0.15

--- core4/__init__.py
+++ core4/__init__.py
@@ -1,6 +1,6 @@
 __project__ = 'core4'
-__version__ = "0.0.14"
-__built__ = "2019-01-17 16:28:27"
+__version__ = "0.0.15"
+__built__ = "2019-01-20 09:51:57"
 name = 'core4'
 title = 'core4 framework'
 description = 'core4 integration automation framework'
