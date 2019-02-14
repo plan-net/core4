79d86647d1b0e3b0054b00f03479cb0b63976dab
m-rau <michi.rau@gmail.com>
2019-02-14 13:57:04 +0100

    bump release-0.0.21

--- core4/__init__.py
+++ core4/__init__.py
@@ -1,6 +1,6 @@
 __project__ = 'core4'
-__version__ = "0.0.20"
-__built__ = "2019-02-13 16:18:20"
+__version__ = "0.0.21"
+__built__ = "2019-02-14 12:57:04"
 name = 'core4'
 title = 'core4 framework'
 description = 'core4 integration automation framework'
