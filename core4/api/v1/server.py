cf8a28d24865db18fb6b346adc75f272f3a71bd2
adi <a.diagne@plan-net.com>
2019-02-20 14:13:32 +0100

    added whitespace to license header

--- core4/api/v1/server.py
+++ core4/api/v1/server.py
@@ -1,9 +1,9 @@
 #
-#Copyright 2018 Plan.Net Business Intelligence GmbH & Co. KG
+# Copyright 2018 Plan.Net Business Intelligence GmbH & Co. KG
 #
-#This Source Code Form is subject to the terms of the Mozilla Public
-#License, v. 2.0. If a copy of the MPL was not distributed with this
-#file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 """
 Delivers the default core4 API server. This server roots to ``/coco/v1``
