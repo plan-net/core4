0c1bdba498aee2bee86eec0568af4fa3d7706914
Marek Mrugalla <m.mrugalla@plan-net.com>
2019-04-01 16:56:26 +0200

    added server.widgets.py, modified package.json

--- core4/api/v1/server.widgets.py
+++ core4/api/v1/server.widgets.py
@@ -6,42 +6,17 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 """
-Delivers the default core4 API server. This server roots to ``/core4/api``
-and provides the following endpoints:
-
-* ``/core4/api/v1/queue`` - :class:`.QueueHandler`
-* ``/core4/api/v1/jobs`` - :class:`.JobHandler`
-* ``/core4/api/v1/jobs/poll`` - :class:`.JobStream`
-* ``/core4/api/v1/enqueue`` - :class:`.JobPost`
-* ``/core4/api/v1/roles`` - :class:`.RoleHandler`
-* ``/core4/api/v1/access`` - :class:`.AcceHandlerr`
-
-Additionally the server creates an endless loop to query collection
-``sys.event`` continuously with :class:`.EventWatch` to support the
-:class:`.EventHandler`.
 
 Start the server with::
     $ python core4/api/v1/server.widget.py
 """
 
-from tornado.ioloop import IOLoop
 
 from core4.api.v1.application import CoreApiContainer
 from core4.api.v1.request.standard.event import EventWatch
 from core4.api.v1.request.standard.event import QueueWatch
 from core4.api.v1.tool.functool import serve
 from core4.api.v1.request.static import CoreStaticFileHandler
-
-
-event = EventWatch()
-IOLoop.current().add_callback(event.watch)
-queue = QueueWatch()
-IOLoop.current().add_callback(queue.watch)
-
-
-def close_watchers():
-    event.change_stream.close()
-
 
 class CoreApiServer(CoreApiContainer):
     """
@@ -50,11 +25,8 @@ class CoreApiServer(CoreApiContainer):
     """
     root = "/core4/api/v1"
     rules = [
-        ("/widgets", CoreStaticFileHandler, {"path": "./request/_static/widgets/dist", "protected": True, "title": "core widgets"})
+        ("/widgets", CoreStaticFileHandler, {"path": "../webapps/widgets/dist", "protected": True, "title": "core widgets"})
     ]
-
-    def on_exit(self):
-        event.change_stream.close()
 
 
 if __name__ == '__main__':
