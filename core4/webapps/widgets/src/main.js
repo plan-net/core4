b11b9c4dd34eabcf30f1866df1ff114addc76e1d
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2019-04-02 10:32:05 +0200

    refactoring widgets, pnbi-base

--- core4/webapps/widgets/src/main.js
+++ core4/webapps/widgets/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import PnbiBase from 'pnbi-base/core4'
-// import 'pnbi-base/core4/themes/pnbi/theme-pnbi.scss'
+import 'pnbi-base/core4/themes/pnbi/theme-pnbi.scss'
 import THEME from 'pnbi-base/core4/themes/pnbi/theme-vuetify'
 import { i18n } from 'pnbi-base/core4/translations'
 import VueDragDrop from 'vue-drag-drop'
@@ -12,10 +12,8 @@ import VueDragDrop from 'vue-drag-drop'
 Vue.use(PnbiBase, {
   router,
   config: {
-    xxxDARK: true,
-    xxxEMAIL: 'bi-ops@plan-net.com',
     THEME,
-    TITLE: 'CORE IV',
+    TITLE: 'CORE4OS',
     IGNORED_ERRORS: [],
     APP_IDENTIFIER: 'core'
   },
