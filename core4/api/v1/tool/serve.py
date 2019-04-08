5ff473c83d2ca5f53822a685fcc9f5e88fb609a2
mra <tad)goW>
2019-04-08 12:32:06 +0200

    improved logging

--- core4/api/v1/tool/serve.py
+++ core4/api/v1/tool/serve.py
@@ -128,9 +128,9 @@ class CoreApiServerTool(CoreBase, CoreLoggerMixin):
         server = tornado.httpserver.HTTPServer(self.router, **http_args)
         server.bind(self.port, address=self.address, reuse_port=reuse_port)
         server.start()
-        self.logger.info("open %ssecure socket on port [%s:%d]",
+        self.logger.info("open %ssecure socket on port [%s:%d] routed at [%s]",
                          "" if http_args.get("ssl_options") else "NOT ",
-                         self.address, self.port)
+                         self.address, self.port, self.routing)
         self.register()
         tornado.ioloop.IOLoop.current().spawn_callback(self.heartbeat)
         try:
