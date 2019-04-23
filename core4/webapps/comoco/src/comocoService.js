67993b04f5e51e50ac5e3fafad8b9c2332c08ca2
oto <o.todorashko@plan-net.com>
2019-04-23 09:53:08 +0200

    [oto.comoco.chart-history]:new: comocoService try catch block

--- core4/webapps/comoco/src/comocoService.js
+++ core4/webapps/comoco/src/comocoService.js
@@ -1,18 +1,26 @@
 import apiService from './apiService.js'
 import { to } from './helper'
 
+var api = new apiService()
+
 export class comocoService extends apiService {
   async getChartHistory () {
     let err
     let setting
     let history;
 
-    [err, setting] = await to(this.getSettings('comoco'))
-    if (!setting) console.log(err);
+    [err, setting] = await to(api.getSettings('comoco'))
+    if (!setting) console.log(err)
+
+    try {
+      setting = await api.getSettings('comoco')
+    } catch (e) {
+      console.log(e)
+    }
 
     // ToDo: get correct start date
 
-    [err, history] = await to(this.getJobsHistory(/* start date */))
+    [err, history] = await to(api.getJobsHistory(/* start date */))
     if (!history) console.log(err)
 
     return history
