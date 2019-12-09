087953ca5e74f9eaa09b4342c073e0fd0e014dc4
m-rau <m.rau@plan-net.com>
2019-12-09 07:09:24 +0100

    bump release-0.4.1

--- core4/__init__.py
+++ core4/__init__.py
@@ -6,8 +6,8 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 __project__ = 'core4'
-__version__ = "0.4.0"
-__built__ = "2019-12-08 22:32:21.976000"
+__version__ = "0.4.1"
+__built__ = "2019-12-09 06:09:24.651000"
 name = 'core4'
 title = 'core4 framework'
 description = 'core4 integration automation framework'
