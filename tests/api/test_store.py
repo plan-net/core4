8a1646d3968f867a40bb08ed6a7fa2d8ddf91384
m-rau <m.rau@plan-net.com>
2020-09-29 00:44:36 +0200

    extended tests with image (logo) data in store

--- tests/api/test_store.py
+++ tests/api/test_store.py
@@ -1,7 +1,8 @@
 from pprint import pprint
-
+import requests
 from tests.api.test_job import worker
 from tests.api.test_test import setup, mongodb, core4api
+import base64
 
 _ = setup
 _ = mongodb
@@ -177,3 +178,54 @@ async def test_skin_example(core4api):
     rv = await core4api.put('/core4/api/v1/store')
     assert rv.code == 200
     pprint(rv.json())
+
+async def test_image_example(core4api):
+    # create user assigned to store "app://store/lufthansa
+    await core4api.login()
+    rv = await core4api.post("/core4/api/v1/roles", body=dict(
+        name="test_role",
+        realname="test role",
+        passwd="test_role",
+        email="test@mail.de",
+        role=["standard_user"],
+        perm=["app://store/lufthansa"]
+    ))
+    assert rv.code == 200
+    rv = await core4api.post("/core4/api/v1/roles", body=dict(
+        name="test_role1",
+        realname="test role1",
+        passwd="test_role1",
+        email="test1@mail.de",
+        role=["standard_user"],
+        perm=[]
+    ))
+    assert rv.code == 200
+    # create default store
+    img = requests.get("https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png")
+    b1 = base64.encodebytes(img.content)
+    rv = await core4api.post(
+        '/core4/api/v1/store', json={"primary": "red", "secondary": "blue", "logo": b1.decode("utf-8")})
+    assert rv.code == 200
+    # create client store overwriting selected values
+
+    img = requests.get("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")
+    b2 = base64.encodebytes(img.content)
+    rv = await core4api.post(
+        '/core4/api/v1/store/lufthansa', json={"primary": "orange", "logo": b2.decode("utf-8")})
+    assert rv.code == 200
+    # login as test user
+    await core4api.login("test_role", "test_role")
+    # access the store automatically routed by app://store/lufthansa key
+    rv = await core4api.put('/core4/api/v1/store')
+    assert rv.code == 200
+    #pprint(rv.json())
+    img = rv.json()["data"]["doc"]["logo"]
+    assert img == b2.decode("utf-8")
+
+    await core4api.login("test_role1", "test_role1")
+    # access the store automatically routed by app://store/lufthansa key
+    rv = await core4api.put('/core4/api/v1/store')
+    assert rv.code == 200
+    #pprint(rv.json())
+    img = rv.json()["data"]["doc"]["logo"]
+    assert img == b1.decode("utf-8")
