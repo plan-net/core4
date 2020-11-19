ad36cc48691b8ab772ee1b76f61b59a60f362f3f
m-rau <m.rau@plan-net.com>
2020-11-19 20:33:34 +0100

    bug: use pql pull request version (required with Python 3.8)

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         "tzlocal>=1.5.1",
         "feather-format==0.4.0",
         "rpy2==3.0.5",
-        "pql>=0.4.3"
+        "git+https://github.com/comfuture/pql.git"
     ],
     extras_require={
         "tests": [
