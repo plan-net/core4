7fa05a134deca58104bfb62cfc264d587ace036e
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2021-01-15 15:08:22 +0100

    bump core4ui

--- core4/webapps/comoco/src/api/api2.js
+++ core4/webapps/comoco/src/api/api2.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Vue from 'vue'
 import { SSE } from '@/components/misc/sse.modified.js'
-
+console.log('process.env.VUE_APP_APIBASE_CORE', process.env.VUE_APP_APIBASE_CORE)
 const on = () => {}
 const off = () => {}
 export function getSSE (
