e7f49b029f31121207b66423960e9c5770f4f064
Marek Mrugalla <m.mrugalla@plan-net.com>
2021-03-23 12:32:15 +0100

    bump release-1.0.3

--- core4/__init__.py
+++ core4/__init__.py
@@ -6,8 +6,8 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 __project__ = 'core4'
-__version__ = "1.0.2"
-__built__ = "2021-03-19 12:54:48.390000"
+__version__ = "1.0.3"
+__built__ = "2021-03-23 11:32:15.273000"
 name = 'core4'
 title = 'core4 framework'
 description = 'core4 integration automation framework'
