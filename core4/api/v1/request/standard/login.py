fb55c282f40184af62332f7680c7a75112dd064a
Luitpold Stotzem <L.Stotzem@house-of-communication.com>
2023-06-19 10:35:17 +0200

    fixed passwort reset bug because of regex-email searching

--- core4/api/v1/request/standard/login.py
+++ core4/api/v1/request/standard/login.py
@@ -188,8 +188,8 @@ class LoginHandler(CoreRequestHandler):
     async def _start_password_reset(self, email):
         # internal method to create and send the password reset token
         self.logger.debug("enter password reset for [%s]", email)
-        email = re.compile(email, re.IGNORECASE)
-        user = await CoreRole().find_one(email=email)
+        regex_email = re.compile(email, re.IGNORECASE)
+        user = await CoreRole().find_one(email=regex_email)
         if user is None:
             self.logger.warning("email [%s] not found", email)
         else:
