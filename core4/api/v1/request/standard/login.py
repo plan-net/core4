0c0fa0ce62c95c1f36c3f41fcfd0ef00dbc100e1
Luitpold Stotzem <L.Stotzem@house-of-communication.com>
2023-07-31 14:27:44 +0200

    removed regex for passwort reset mail

--- core4/api/v1/request/standard/login.py
+++ core4/api/v1/request/standard/login.py
@@ -188,7 +188,7 @@ class LoginHandler(CoreRequestHandler):
     async def _start_password_reset(self, email):
         # internal method to create and send the password reset token
         self.logger.debug("enter password reset for [%s]", email)
-        regex_email = re.compile(email, re.IGNORECASE)
+        regex_email = email.lower()
         user = await CoreRole().find_one(email=regex_email)
         if user is None:
             self.logger.warning("email [%s] not found", email)
